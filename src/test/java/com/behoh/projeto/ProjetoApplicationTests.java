d16aada1481576fac144d8bd5a468897d7622c3d
EstomJr <estonjr2@hotmail.com>
2020-10-01 10:49:56 -0300

    Projeto criado

---
+++ src/test/java/com/behoh/projeto/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behoh.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
